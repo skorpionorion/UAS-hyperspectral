3c2a7c418c9b83ed9f6274e0f64c73f429f3c537
skorpionorion <warit.marc@gmail.com>
2025-11-19 23:30:44 -0500

    Update MarcCNNtest.py

--- MarcTest/MarcCNNtest.py
+++ MarcTest/MarcCNNtest.py
@@ -18,8 +18,8 @@ X = Signals[list(Signals.keys())[-1]].T
 y = Moisture_Percentage[list(Moisture_Percentage.keys())[-1]].T
 
 # Normalize
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+x_scaler = StandardScaler()
+X_scaled = x_scaler.fit_transform(X)
 
 #Split into training and testing sets (70/30)
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
@@ -31,20 +31,20 @@ print(X_train.min())
 print(y_train.max())
 print(y_train.min())
 
-
 ## Build the model
 # CNN Model
 X_train_cnn = X_train[..., np.newaxis]
 X_test_cnn = X_test[..., np.newaxis]
 
 cnn_model = Sequential([
-    Conv1D(64, 5, activation='relu', input_shape=(X_train_cnn.shape[1], 1)), # Conv1D layer
+    Conv1D(64, 3, activation='relu', input_shape=(X_train_cnn.shape[1], 1)), # Conv1D layer
     Conv1D(32, 3, activation='relu'),
+    Dropout(0.1), # add dropout after the first convolutional layer
     Conv1D(16, 3, activation='relu'),
-    Flatten(),
-    Dense(64, activation='relu'), # Dense layer
-    Dense(32, activation='relu'),
-    Dropout(0.5),
+    Flatten(), # flattne the output
+    Dense(128, activation='relu'), # Dense layer
+    Dense(64, activation='relu'),
+    Dropout(0.1), # add dropout after the dense layer
     Dense(1, activation='linear')
 ])
 
@@ -52,32 +52,30 @@ cnn_model.summary()
 
 ## Train the model
 batch_size = 8
-epochs = 300
+epochs = 200
 
-cnn_model.compile(optimizer=Adam(1e-4), loss='mse', metrics=['mae'])
+cnn_model.compile(optimizer=Adam(1e-3), loss='mse', metrics=['mae'])
 hist = cnn_model.fit(X_train_cnn, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.2, verbose=1)
 
 # Evaluate CNN Model
-y_pred_cnn = cnn_model.predict(X_test_cnn).flatten()
+y_pred_cnn= cnn_model.predict(X_test_cnn)
+
+# MSE and Mean Absolute Error
+print("\n1D CNN Model Performance")
 mae_cnn = mean_absolute_error(y_test, y_pred_cnn)
-rmse_cnn = np.sqrt(mean_squared_error(y_test, y_pred_cnn))
+mse_cnn = mean_squared_error(y_test, y_pred_cnn)
 
 ## RMSE and R2 score
-score = cnn_model.evaluate(X_test_cnn, y_test, verbose=0)
-print("Test MSE:", score[0])
-print("Test Mean Absolute Error:", score[1])
-
-
+rmse_cnn = np.sqrt(mse_cnn)
 r2_test = r2_score(y_test, y_pred_cnn)
-print("Test R2 Score:", r2_test)
 
 # Results
-print("\n1D CNN Model Performance")
-print(f"RMSE = {rmse_cnn:.6f}")
-
+print("Test R2 Score:", r2_test)
+print("Test MSE:", mse_cnn)
+print("Test Mean Absolute Error:", mae_cnn)
+print("RMSE:", rmse_cnn)
 # Plot training & validation MAE values
 plt.figure(figsize=(10, 4))
-
 plt.subplot(1, 2, 1)
 plt.plot(hist.history["mae"])
 plt.plot(hist.history["val_mae"])
@@ -107,6 +105,20 @@ plt.plot(y_pred_cnn, 's--', label='Predicted Moisture', markersize=6)
 plt.xlabel('Sample Index')
 plt.ylabel('Moisture Percentage')
 plt.title('Predicted vs Actual Moisture on Test Set')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+# Plotting Predicted vs Actual field moisture content
+plt.figure(figsize=(10,6))
+plt.scatter(y_test, y_pred_cnn)
+min_val = min(y_test.min(), y_pred_cnn.min())
+max_val = max(y_test.max(), y_pred_cnn.max())
+plt.plot([min_val, max_val], [min_val, max_val], 'r--', label='Perfect Prediction')
+plt.xlabel('Actual FMC')
+plt.ylabel('Predicted FMC')
+plt.title('Predicted vs Actual FMC')
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
